38f8b02093f0da8ca265788c40c4005452a4cbc8
elvar333 <elvararnibjarnason@gmail.com>
2018-10-03 20:33:33 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let countDownDate = new Date("Dec 24, 2018 18:00:00 GMT+0000").getTime();
+
+let x = setInterval(function () {
+    let now = new Date().getTime();
+
+    let distance = countDownDate - now;
+
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / (1000));
+
+    document.getElementById("timer").innerHTML =
+        days + "d " + hours + "klst " + minutes + "m " + seconds + "s ";
+}, 1000);
